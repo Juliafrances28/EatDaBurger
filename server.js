ffde15b7089b24c44bb24fe2b699a7e2e61a4c1f
Julia Horan <julia.horan28@gmail.com>
2020-12-09 11:06:50 -0500

    errors fixed

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-
 var express = require("express");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8081;
 
 var app = express();
 
@@ -18,7 +17,7 @@ var routes = require("./controllers/burgerController.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
